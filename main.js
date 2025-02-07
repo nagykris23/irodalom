27f1331436140de84efafb145e2be11c0beac376
Kristof <nagykris68@gmail.com>
2025-02-07 21:37:43 +0100

    változók bevezetése

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+const fejlecElso = "Szerző neve"; // Fejléc szövege
+const fejlecMasodik = "Korszak"; // Fejléc szövege
+const fejlecHarmadik = "Szerelmek"; // Fejléc szövege
+const elsosrocella1 = "Balassi Bálint"; // Első cella szövege az első sorban
+const elsosorcella2 = "reformáció"; // Második cella szövege az első sorban
+const elsosorcella3 = "Losonczy Anna"; // Harmadik cella szövege az első sorban
+const elsosorcella4 = "Dobó Krisztina"; // Negyedik cella szövege az első sorban
+const masodiksorcella1 = "Csokonai Vitéz Mihály"; // Első cella szövege a második sorban
+const masodiksorcella2 = "felvilágosodás"; // Második cella szövege a második sorban
+const masodiksorcella3 = "Vajda Juliána"; // Harmadik cella szövege a második sorban
+const harmadiksorcella1 = "Petőfi Sándor"; // Első cella szövege a harmadik sorban
+const harmadiksorcella2 = "magyar romantika"; // Második cella szövege a harmadik sorban
+const harmadiksorcella3 = "Mednyánszky Berta"; // Harmadik cella szövege a harmadik sorban
+const harmadiksorcella4 = "Szendrey Júlia"; // Negyedik cella szövege a harmadik sorban
+const negyediksorcella1 = "Ady Endre"; // Első cella szövege a negyedik sorban
+const negyediksorcella2 = "20. század"; // Második cella szövege a negyedik sorban
+const negyediksorcella3 = "Léda"; // Harmadik cella szövege a negyedik sorban
+const negyediksorcella4 = "Csinszka"; // Negyedik cella szövege a negyedik sorban
+
 const table = document.createElement("table")//table elem létrehozása
 document.body.appendChild(table)//table appendChildolása a bodyhoz
 
@@ -5,38 +24,38 @@ const tablehead = document.createElement('tr') // tr fejléc létrehozása
 table.appendChild(tablehead)// a  sor hozzáadása a táblázasthoz
 
 const thszerzo = document.createElement('th')// fejléc cella létrehozása
-thszerzo.innerHTML = 'Szerző neve' // megadjuk a cella tartalmát
+thszerzo.innerHTML = fejlecElso // megadjuk a cella tartalmát
 tablehead.appendChild(thszerzo)// cella hozzá adása a fejléc sorához
 
 const thkorszak = document.createElement('th')// újabb fejléc cella létrehozása
-thkorszak.innerHTML = 'Korszak' //megadjuk a cella tartalmát
+thkorszak.innerHTML = fejlecMasodik //megadjuk a cella tartalmát
 tablehead.appendChild(thkorszak)//cella hozzá adása a fejléc sorához
 
 const thszerelem1 = document.createElement('th')//még egy fejléc cella létrehozása
-thszerelem1.innerHTML = "Szerelmek"//megadjuk a cella tartalmát
+thszerelem1.innerHTML = fejlecHarmadik//megadjuk a cella tartalmát
 tablehead.appendChild(thszerelem1)//cella hozzá adása a fejléc sorához
 
 const thszerelem2 = document.createElement('th')//második szerelmének a fejléc cella létrehozása
-thszerelem2.innerHTML = "Szerelmek"// megadjuk a cella tartalmát
+thszerelem2.innerHTML = fejlecHarmadik// megadjuk a cella tartalmát
 tablehead.appendChild(thszerelem2)//cella hozzá adása a fejléc sorához
 
 const elsosor = document.createElement('tr')//táblázat sorának létrehozása az adatoknak
 table.appendChild(elsosor)//hozzá adjuk a táblázathoz a sort
 
 const elsosorSzerzo = document.createElement('td')//cella létrehozása a szerzőnek
-elsosorSzerzo.innerHTML = 'Balasi Bálint'//cella tartalmát beállítja
+elsosorSzerzo.innerHTML = elsosrocella1//cella tartalmát beállítja
 elsosor.appendChild(elsosorSzerzo)//cella hozzá adása az első sorhoz
 
 const elsosorKorszak = document.createElement('td')//cella létrehozása a korszaknak
-elsosorKorszak.innerHTML='reformáció'//cella tartalmát beállítja
+elsosorKorszak.innerHTML=elsosorcella2//cella tartalmát beállítja
 elsosor.appendChild(elsosorKorszak)//cella hozzá adása az első sorhoz
 
 const elsosorSzerelem1 = document.createElement('td')//cella létrhozása a szerelmének
-elsosorSzerelem1.innerHTML='Losonczy Anna'//cella tartalmát beállítja
+elsosorSzerelem1.innerHTML=elsosorcella3//cella tartalmát beállítja
 elsosor.appendChild(elsosorSzerelem1)//cella hozzá adása az első sorhoz
 
 const elsosorSzerelem2 = document.createElement('td')//cella létrehozása a szerelmének
-elsosorSzerelem2.innerHTML='Dobó Krisztina'//cellla tartalmát beálítja
+elsosorSzerelem2.innerHTML=elsosorcella4//cellla tartalmát beálítja
 elsosor.appendChild(elsosorSzerelem2)//cella hozzá adása az első sorhoz
 
 
@@ -44,15 +63,15 @@ const masodiksor = document.createElement('tr')//táblázat sorának létrehozá
 table.appendChild(masodiksor)//hozzá adjuk a táblázathoz a sort
 
 const masodiksosorSzerzo = document.createElement('td')//cella létrehozása a szerzőnek
-masodiksosorSzerzo.innerHTML = 'Csokonai Vitéz Mihály'//cella tartalmát beállítja
+masodiksosorSzerzo.innerHTML = masodiksorcella1//cella tartalmát beállítja
 masodiksor.appendChild(masodiksosorSzerzo)//cella hozzá adása az második sorhoz
 
 const masodiksorKorszak = document.createElement('td')//cella létrehozása a korszaknak
-masodiksorKorszak.innerHTML='felvilágosodás'//cella tartalmát beállítja
+masodiksorKorszak.innerHTML= masodiksorcella2//cella tartalmát beállítja
 masodiksor.appendChild(masodiksorKorszak)//cella hozzá adása az második sorhoz
 
 const masodiksorSzerelem1 = document.createElement('td')//cella létrhozása a szerelmének
-masodiksorSzerelem1.innerHTML='Vajda Juliána'//cella tartalmát beállítja
+masodiksorSzerelem1.innerHTML= masodiksorcella3//cella tartalmát beállítja
 masodiksor.appendChild(masodiksorSzerelem1)//cella hozzá adása az második sorhoz
 
 const masodiksorSzerelem2 = document.createElement('td')//cella létrehozása a szerelmének
@@ -64,36 +83,36 @@ const harmadiksor = document.createElement('tr')//táblázat sorának létrehoz�
 table.appendChild(harmadiksor)//hozzá adjuk a táblázathoz a sort
 
 const harmadiksorSzerzo = document.createElement('td')//cella létrehozása a szerzőnek
-harmadiksorSzerzo.innerHTML = 'Petőfi Sándor'//cella tartalmát beállítja
+harmadiksorSzerzo.innerHTML = harmadiksorcella1//cella tartalmát beállítja
 harmadiksor.appendChild(harmadiksorSzerzo)//cella hozzá adása az harmadik sorhoz
 
 const harmadiksorKorszak = document.createElement('td')//cella létrehozása a korszaknak
-harmadiksorKorszak.innerHTML='magyar romantika'//cella tartalmát beállítja
+harmadiksorKorszak.innerHTML=harmadiksorcella2//cella tartalmát beállítja
 harmadiksor.appendChild(harmadiksorKorszak)//cella hozzá adása az harmadik sorhoz
 
 const harmadiksorSzerelem1 = document.createElement('td')//cella létrhozása a szerelmének
-harmadiksorSzerelem1.innerHTML='Mednyászky Berta'//cella tartalmát beállítja
+harmadiksorSzerelem1.innerHTML=harmadiksorcella3//cella tartalmát beállítja
 harmadiksor.appendChild(harmadiksorSzerelem1)//cella hozzá adása az harmadik sorhoz
 
 const harmadiksorSzerelem2 = document.createElement('td')//cella létrehozása a szerelmének
-harmadiksorSzerelem2.innerHTML='Szendrey Júlia'//cellla tartalmát beálítja 
+harmadiksorSzerelem2.innerHTML=harmadiksorcella4//cellla tartalmát beálítja 
 harmadiksor.appendChild(harmadiksorSzerelem2)//cella hozzá adása az harmadik sorhoz
 
 const negyediksor = document.createElement('tr')//táblázat sorának létrehozása az adatoknak
 table.appendChild(negyediksor)//hozzá adjuk a táblázathoz a sort
 
 const negyediksorSzerzo = document.createElement('td')//cella létrehozása a szerzőnek
-negyediksorSzerzo.innerHTML = 'Ady Endre'//cella tartalmát beállítja
+negyediksorSzerzo.innerHTML = negyediksorcella1//cella tartalmát beállítja
 negyediksor.appendChild(negyediksorSzerzo)//cella hozzá adása az negyedik sorhoz
 
 const negyediksorKorszak = document.createElement('td')//cella létrehozása a korszaknak
-negyediksorKorszak.innerHTML='20. század'//cella tartalmát beállítja
+negyediksorKorszak.innerHTML=negyediksorcella2//cella tartalmát beállítja
 negyediksor.appendChild(negyediksorKorszak)//cella hozzá adása az negyedik sorhoz
 
 const negyediksorSzerelem1 = document.createElement('td')//cella létrhozása a szerelmének
-negyediksorSzerelem1.innerHTML='Léda'//cella tartalmát beállítja
+negyediksorSzerelem1.innerHTML=negyediksorcella3//cella tartalmát beállítja
 negyediksor.appendChild(negyediksorSzerelem1)//cella hozzá adása az negyedik sorhoz
 
 const negyediksorSzerelem2 = document.createElement('td')//cella létrehozása a szerelmének
-negyediksorSzerelem2.innerHTML='Csinszka'//cellla tartalmát beálítja 
+negyediksorSzerelem2.innerHTML=negyediksorcella4//cellla tartalmát beálítja 
 negyediksor.appendChild(negyediksorSzerelem2)//cella hozzá adása az negyedik sorhoz
